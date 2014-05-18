7c742d4f7c065c52c7fa9d515dee12a5a9869a55
schqiushui <orochi9999@gmail.com>
2014-05-18 15:36:31 +0800

    Unlock the Governor

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -427,8 +427,8 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	char	str_governor[16];
 	struct cpufreq_policy new_policy;
 
-	if (sys_locked)
-		return -EINVAL;
+	//if (sys_locked)
+	//	return -EINVAL;
 
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);
 	if (ret)
@@ -657,6 +657,7 @@ static struct attribute_group vddtbl_attr_group = {
 };
 #endif	/* CONFIG_CPU_VOLTAGE_TABLE */
 
+#ifdef CONFIG_CMDLINE_OPTIONS 
 /* Read cmdline for sys_locked */
 static int __init read_sys_locked_cmdline(char *locked)
 {
@@ -673,7 +674,7 @@ static int __init read_sys_locked_cmdline(char *locked)
 }
 
 __setup("locked=", read_sys_locked_cmdline);
-
+#endif /* CONFIG_CMDLINE_OPTIONS */
 
 static ssize_t show_locked(struct kobject *a, struct attribute *b, char *buf)
 {
